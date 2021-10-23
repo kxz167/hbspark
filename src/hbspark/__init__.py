dd80d900ff2d360af5b706a6e8489ec225f69803
kris zhao <kxz167@case.edu>
2021-10-22 23:22:04 -0400

    corrected connecion

--- src/hbspark/__init__.py
+++ src/hbspark/__init__.py
@@ -1,8 +1,10 @@
 import example
 import happybase as hb
 
+#Instantiation of the HB session:
 hb_session = None
 
+# Initialize the connection:
 def connect(hostname):
     hb_session = Session(hb.Connection(hostname))
 
@@ -10,9 +12,10 @@ def tables(hostname):
     if(hb_session):
         return hb_session.connection().tables()
 
+# Manages open connection to happybase in order to make various queries.
 class Session:
     def __init__(self, conn):
         self.conn = conn
 
-    def connection():
+    def connection(self):
         return self.conn
