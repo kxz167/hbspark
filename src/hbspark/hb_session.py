6753419f870e5c43417555033a18e0c4a01653e4
kris zhao <kxz167@case.edu>
2021-10-23 01:01:56 -0400

    added connection link

--- src/hbspark/hb_session.py
+++ src/hbspark/hb_session.py
@@ -2,6 +2,10 @@ def init(connection):
     global hb_session
     hb_session = Session(connection)
 
+def connection():
+    global hb_session
+    return hb_session.connection()
+
 # Manages open connection to happybase in order to make various queries.
 class Session:
     def __init__(self, conn):
